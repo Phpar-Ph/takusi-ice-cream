4641220742f3da6bf2a835aa06bb9c373b9f5946
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-02-27 04:00:32 +0800

    Adding carousel /enhancing features

--- src/pages/Flavor.jsx
+++ src/pages/Flavor.jsx
@@ -5,19 +5,21 @@ import FlavorGallery from '../components/FlavorGallery'
 
 function Flavor() {
   return (
-    <div className='h-full w-full bg-blue '>
+    <div className='h-screen w-full bg-blue '>
       {/* FLAVOR HEADING */}
-        <div className='h-[160px] w-full bg-brown relative '>
-            <div className='px-[142px]'>
-              {/* FLAVOR HEADER */}
-                <div className=' font-neuton text-white text-right flex flex-col  justify-end py-4'>
-                    <h1 className='pr-18 text-4xl'>Our</h1>
-                    <h1 className=' text-7xl  pr-10'>FLAVORS</h1>
-                </div>
-                {/* IMAGE */}
-                <div className='absolute bottom-0'>
-                  <img src={Hand} alt="Takusi Ice Cream Logo" className="h-30"  />
-                </div>
+        <div className='h-[160px]  bg-brown relative '>
+            <div className='max-w-[1920px] mx-auto'>
+              <div className='px-[142px]'>
+                {/* FLAVOR HEADER */}
+                  <div className=' font-neuton text-white text-right flex flex-col  justify-end py-4'>
+                      <h1 className='pr-18 text-4xl'>Our</h1>
+                      <h1 className=' text-7xl  pr-10'>FLAVORS</h1>
+                  </div>
+                  {/* IMAGE */}
+                  <div className='absolute bottom-0'>
+                    <img src={Hand} alt="Takusi Ice Cream Logo" className="h-30"  />
+                  </div>
+              </div>
             </div>
         </div>
        
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,25 +6,27 @@ import Strawberry2 from '../assets/Strawberry2.png'
 
 function Home() {
   return (
-    <div className='h-[95vh] w-full bg-yellow'>
+    <div className='h-screen w-full bg-yellow'>
       {/* <div className="absolute bg-black opacity-20"></div> */}
-      <div className='grid place-items-center h-[95vh]'>
-          <div className=' top-34 relative'>
-            <img src={Image} alt='Ice cream' className='h-[1046px' />
-          </div>
-          <div className='relative grid place-items-center top-[-40%] ' >
-            <h1 className='text-8xl pb-4 m-4 font-pacifico text-white'>Fresh, Healthy, Naturally Delicious.</h1>
-              <div className='flex'>
-                <div>
-                <img src={Strawberry1} alt='Strawberry' className='h-[200px]' />
-                </div>
-              <p className='text-3xl w-[774px] text-center font-neuton text-brown'>Discover the joy of ice cream crafted with natural flavors, delivering a fresh, healthy, and irresistibly delicious treat in every scoop.</p>
-              <div>
-                <img src={Strawberry2} alt='Strawberry' className='h-[200px]' />
-                </div>
+        <div className='max-w-[1920px] mx-auto'>
+            <div className='grid place-items-center h-[95vh]'>
+              <div className=' top-34 relative'>
+                <img src={Image} alt='Ice cream' className='h-[1046px' />
+              </div>
+              <div className='relative grid place-items-center top-[-40%] ' >
+                <h1 className='text-8xl pb-4 m-4 font-pacifico text-white'>Fresh, Healthy, Naturally Delicious.</h1>
+                  <div className='flex'>
+                    <div>
+                    <img src={Strawberry1} alt='Strawberry' className='h-[200px]' />
+                    </div>
+                  <p className='text-3xl w-[774px] text-center font-neuton text-brown'>Discover the joy of ice cream crafted with natural flavors, delivering a fresh, healthy, and irresistibly delicious treat in every scoop.</p>
+                  <div>
+                    <img src={Strawberry2} alt='Strawberry' className='h-[200px]' />
+                    </div>
+                  </div>
               </div>
           </div>
-      </div>
+        </div>
     </div>
   )
 }
